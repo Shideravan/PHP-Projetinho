f0130e1f6ad09c9086efebb47d222b5bcda177ff
Shideravan <spannopan@yahoo.com.br>
2021-02-11 18:01:13 -0300

    Adicionando operador ternário

--- 6-decisoes.php
+++ 6-decisoes.php
@@ -2,7 +2,8 @@
 
 $idade = 111;
 $nome = "oi";
-echo "Você só pode entrar se tiver mais do que 18 anos." . PHP_EOL;
+echo "Você só pode entrar se tiver mais do que 18 anos ";
+echo "ou a partir de 16 anos acompanhado." . PHP_EOL;
 
 if ($idade > 18 && $nome == "oi") {
     echo "Você tem $idade anos. Pode entrar";
@@ -15,3 +16,8 @@ if ($idade > 18 or $idade == 18) {
 } else {
     echo "Você não pode entrar. Menor de idade...";
 }
+
+//usando operador ternário
+$idade = 15;
+$mensagem = $idade < 18 ? ‘Você é menor de idade’ : ‘Você é maior de idade’;
+echo $mensagem;
