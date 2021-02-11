eee678bb7829592e5ab9ca415c1fd77d684c9832
Shideravan <spannopan@yahoo.com.br>
2021-02-11 18:32:39 -0300

    até o 8

--- 8-repeticao-2.php
+++ 8-repeticao-2.php
@@ -1,5 +1,10 @@
 <?php
 
-for ($i = 1; $i <= 10; $i++) {
+for ($i = 1; $i <= 20; $i++) {
+    if ($i == 10) {
+        continue; //<-Fará pular apenas o 10 e o resto irá de boa
+    } else if ($i == 16) {
+        break; //<-vai TERMINAR o bloco FOR todo quando chegar no 16, ou seja, vai listar só até 15;
+    }
     echo "#" . $i . PHP_EOL;
 }
