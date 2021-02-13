86be286069b7f0bf33987da930f0b4cd328e5631
Shideravan <spannopan@yahoo.com.br>
2021-02-12 23:13:43 -0300

    Add forma extra de resolver

--- Desafios/4-desafio-placa.php
+++ Desafios/4-desafio-placa.php
@@ -14,3 +14,26 @@ $carro1 = [
 ];
 
 echo $carro1['placa'];
+
+//outra implementação
+echo PHP_EOL;
+echo PHP_EOL;
+
+$carros = [
+    'LMS-2232' => [
+        'marca' => 'VW',
+        'modelo' => 'Golf'
+    ],
+    'KLM-1324' => [
+        'marca' => 'Ford',
+        'modelo' => 'Fiesta'
+    ],
+    'OPN-5612' => [
+        'marca' => 'Fiat',
+        'modelo' => 'Uno'
+    ],
+];
+
+foreach ($carros as $placa => $carro) {
+    echo $placa . ': ' . $carro['marca'] . PHP_EOL;
+}
